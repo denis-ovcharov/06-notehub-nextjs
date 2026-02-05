780f9a27a18251a352080012d0070f53b3313278
Denys Ovcharov <denisovcharov88@gmail.com>
2026-02-05 14:56:13 +0100

    'update'

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,24 +1,71 @@
-import { fetchNotes } from "@/lib/api";
+"use client";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../lib/api";
+import NoteList from "../../components/NoteList/NoteList";
+import Pagination from "../../components/Pagination/Pagination";
+import css from "./page.module.css";
 import { useState } from "react";
+import Modal from "../../components/Modal/Modal";
+import NoteForm from "../../components/NoteForm/NoteForm";
+import { Toaster } from "react-hot-toast";
 import { useDebouncedCallback } from "use-debounce";
+import SearchBox from "../../components/SearchBox/SearchBox";
 
-function Notes() {
-    const [query, setQuery] = useState("");
-      const [page, setPage] = useState(1);
-      const [isModalOpen, setIsModalOpen] = useState(false);
-    
-      const { data, isSuccess } = useQuery({
-        queryKey: ["notes", query, page],
-        queryFn: () => fetchNotes(query, page),
-        placeholderData: keepPreviousData,
-      });
-      const updateQuery = useDebouncedCallback(
+function NotesClient() {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["notes", query, page],
+    queryFn: () => fetchNotes(query, page),
+    placeholderData: keepPreviousData,
+  });
+
+  const updateQuery = useDebouncedCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       setQuery(e.target.value);
       setPage(1);
     },
     1000,
   );
-    return ()
+
+  const notes = data?.notes || [];
+  const totalPages = data?.totalPages ?? 1;
+
+  const handleOpenCreateModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          <SearchBox onSearch={updateQuery} />
+          {totalPages > 1 && (
+            <Pagination
+              totalPages={totalPages}
+              page={page}
+              onPageChange={setPage}
+            />
+          )}
+          <button className={css.button} onClick={handleOpenCreateModal}>
+            Create note +
+          </button>
+        </header>
+        {notes.length > 0 && isSuccess && <NoteList notes={notes} />}
+      </div>
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <NoteForm onClose={handleCloseModal} />
+        </Modal>
+      )}
+      <Toaster position="top-right" />
+    </>
+  );
 }
+
+export default NotesClient;
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,53 +1,5 @@
-"use client";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { fetchNotes } from "../../lib/api";
-import NoteList from "../../components/NoteList/NoteList";
-import Pagination from "../../components/Pagination/Pagination";
-import css from "./page.module.css";
-import { useState } from "react";
-import Modal from "../../components/Modal/Modal";
-import NoteForm from "../../components/NoteForm/NoteForm";
-import { Toaster } from "react-hot-toast";
-import { useDebouncedCallback } from "use-debounce";
-import SearchBox from "../../components/SearchBox/SearchBox";
+import NotesClient from "./Notes.client";
 
-function Notes() {
-  const notes = data?.notes || [];
-  const totalPages = data?.totalPages ?? 1;
-
-  const handleOpenCreateModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-  return (
-    <>
-      <div className={css.app}>
-        <header className={css.toolbar}>
-          <SearchBox onSearch={updateQuery} />
-          {totalPages > 1 && (
-            <Pagination
-              totalPages={totalPages}
-              page={page}
-              onPageChange={setPage}
-            />
-          )}
-          <button className={css.button} onClick={handleOpenCreateModal}>
-            Create note +
-          </button>
-        </header>
-        {notes.length > 0 && isSuccess && <NoteList notes={notes} />}
-      </div>
-      {isModalOpen && (
-        <Modal onClose={handleCloseModal}>
-          <NoteForm onClose={handleCloseModal} />
-        </Modal>
-      )}
-      <Toaster position="top-right" />
-    </>
-  );
+export default function Page() {
+  return <NotesClient />;
 }
-
-export default Notes;
